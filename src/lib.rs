bc45e5a6ed303297a04def9268c884cd8e165c71
Michael Guzman <mgzmn4@gmail.com>
2025-08-29 06:01:49 -0500

    refactored to use iterators

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,8 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -45,10 +40,10 @@ Duct tape";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three
-Trus me.";
+Pick three.
+Trust me.";
         assert_eq!(
-            vec!["Rust", "Trust me."],
+            vec!["Rust:", "Trust me."],
             search_case_insensitive(query, contents)
         );
     }
